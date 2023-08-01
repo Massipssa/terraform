535cbb913e849e06ca53fa90e734a46c7bff7092
Massipssa Kerrache <kerrache.massipssa@gmail.com>
2023-08-01 10:58:41 +0200

    Update gitignore

--- dev/gcp/variables.tf
+++ dev/gcp/variables.tf
@@ -49,3 +49,15 @@ variable "vm_types" {
     small = "e2-micro"
   }
 }
+
+## IAM 
+variable "var_name" {
+  type = string
+  description = "This is description of the variable"
+  default = "default-value"
+
+  validation {
+    condition = length(var.var_name) > 4 && substr(var.var_name, 0, 4) == "ami-"
+    error_message = "This is not valid AMI id"
+  }
+}
